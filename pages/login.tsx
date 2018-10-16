9f157b0391a7f68356382f23181d285213a36c4d
dacasas <dacasas@uc.cl>
2018-10-16 09:23:17 -0300

    Login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,21 +1,153 @@
 import React from "react"
-import Link from "next/link"
+import Avatar from "@material-ui/core/Avatar"
+import Button from "@material-ui/core/Button"
+import CssBaseline from "@material-ui/core/CssBaseline"
+import FormControl from "@material-ui/core/FormControl"
+import FormControlLabel from "@material-ui/core/FormControlLabel"
+import Checkbox from "@material-ui/core/Checkbox"
+import Input from "@material-ui/core/Input"
+import InputLabel from "@material-ui/core/InputLabel"
+import LockIcon from "@material-ui/icons/LockOutlined"
+import Paper from "@material-ui/core/Paper"
+import Typography from "@material-ui/core/Typography"
+import withStyles from "@material-ui/core/styles/withStyles"
+
+const styles = theme => ({
+  layout: {
+    width: "auto",
+    display: "block", // Fix IE11 issue.
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 400,
+      marginLeft: "auto",
+      marginRight: "auto",
+    },
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 8,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme
+      .spacing.unit * 3}px`,
+  },
+  avatar: {
+    margin: theme.spacing.unit,
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE11 issue.
+    marginTop: theme.spacing.unit,
+  },
+  submit: {
+    marginTop: theme.spacing.unit * 3,
+  },
+})
 
 export interface LoginPageProps {}
 
-export interface LoginPageState {}
+export interface LoginPageState {
+  email: string
+  password: string
+  remember: boolean
+}
 
 class LoginPage extends React.Component<LoginPageProps, LoginPageState> {
+  state = {
+    email: "",
+    password: "",
+    remember: false,
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    console.log("user: ", this.state.email)
+    console.log("password: ", this.state.password)
+    console.log("remember: ", this.state.remember)
+    console.log("valid: ", this.validateForm())
+  }
+
+  validateForm = () => {
+    return this.state.email.length > 0 && this.state.password.length > 0
+  }
+
+  handleChangePass = password => {
+    this.setState({ password })
+  }
+
+  handleChangeEmail = email => {
+    this.setState({ email })
+  }
+
+  handleChangeRemember = remember => {
+    this.setState({ remember })
+  }
+
   render() {
+    const { classes } = this.props
+
     return (
       <React.Fragment>
-        <h1>Hello World!</h1>
-        <Link href="/">
-          <button> go to home page!</button>
-        </Link>
+        <CssBaseline />
+        <main className={classes.layout}>
+          <Paper className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <form className={classes.form} onSubmit={this.handleSubmit}>
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="email">Email Address</InputLabel>
+                <Input
+                  id="email"
+                  value={this.state.email}
+                  onChange={e => this.handleChangeEmail(e.target.value)}
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                />
+              </FormControl>
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="password">Password</InputLabel>
+                <Input
+                  name="password"
+                  type="password"
+                  id="password"
+                  value={this.state.password}
+                  onChange={e => this.handleChangePass(e.target.value)}
+                  autoComplete="current-password"
+                />
+              </FormControl>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    id="remember"
+                    value="remember"
+                    checked={this.state.remember}
+                    onChange={e => this.handleChangeRemember(e.target.checked)}
+                    color="primary"
+                  />
+                }
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Sign in
+              </Button>
+            </form>
+          </Paper>
+        </main>
       </React.Fragment>
     )
   }
 }
 
-export default LoginPage
+export default withStyles(styles)(LoginPage)
